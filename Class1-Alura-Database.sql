d8f12e81113bd545df614540483e3592728a6b67
Yuri Cruz <yuricruzf@gmail.com>
2023-08-30 22:56:08 -0300

    Foreign keys

--- Class1-Alura-Database.sql
+++ Class1-Alura-Database.sql
@@ -193,3 +193,45 @@ INSERT INTO curso (id, nome) VALUES (1, 'HTML');
 INSERT INTO curso (id, nome) VALUES (2, 'JAVASCRIPT'); -- Não vai, pois o ID tá duplicado
 
 SELECT * FROM curso;
+
+-- FOREIGN KEY
+CREATE TABLE student (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL
+);
+
+INSERT INTO student (nome) VALUES('Diogo');
+INSERT INTO student (nome) VALUES('Vinicius');
+
+SELECT * FROM student;
+SELECT * FROM curso;
+
+DROP TABLE student_curso;
+CREATE TABLE student_curso (
+	student_id INTEGER,
+	curso_id INTEGER,
+	PRIMARY KEY(student_id, curso_id), -- Chave primária composta
+	
+	FOREIGN KEY (student_id) --Pegando a chave da tabela
+	REFERENCES student(id), -- Referenciando com a chave ID da tabela externa student
+	
+	FOREIGN KEY (curso_id)
+	REFERENCES curso(id)
+);
+
+INSERT INTO student_curso (student_id, curso_id) VALUES(1,1);
+INSERT INTO student_curso (student_id, curso_id) VALUES(2,1);
+
+INSERT INTO student_curso (student_id, curso_id) VALUES(3,1); --Nenhuma solução abaixo funciona, pois ou o aluno ou o curso 3 não existe
+INSERT INTO student_curso (student_id, curso_id) VALUES(1,3);
+
+SELECT * FROM student WHERE id= 1;
+SELECT * FROM curso WHERE id = 1;
+
+SELECT * FROM student WHERE id= 2;
+SELECT * FROM curso WHERE id = 1;
+
+SELECT * FROM student WHERE id= 3;
+SELECT * FROM curso WHERE id = 1;
+
+SELECT * FROM student_curso;
